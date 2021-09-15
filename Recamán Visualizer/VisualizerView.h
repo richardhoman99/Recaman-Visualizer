a2f6063af6dab1d5fc31d00625343c4b4831d8f7
richardhoman99 <rhoman99@icloud.com>
2021-09-14 23:06:44 -0500

    Initial commit

---
+++ Recamán Visualizer/VisualizerView.h
@@ -0,0 +1,16 @@
+//
+//  VisualizerView.h
+//  Recamán Visualizer
+//
+//  Created by Richard Homan on 9/14/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VisualizerView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Recamán Visualizer/VisualizerView.m
@@ -0,0 +1,18 @@
+//
+//  VisualizerView.m
+//  Recamán Visualizer
+//
+//  Created by Richard Homan on 9/14/21.
+//
+
+#import "VisualizerView.h"
+
+@implementation VisualizerView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
