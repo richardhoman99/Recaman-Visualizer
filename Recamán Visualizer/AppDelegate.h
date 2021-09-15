4499b09434c2e26af0801087eb8978832a807cf2
richardhoman99 <rhoman99@icloud.com>
2021-09-14 23:04:20 -0500

    Initial Commit

---
+++ Recamán Visualizer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Recamán Visualizer
+//
+//  Created by Richard Homan on 9/14/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Recamán Visualizer/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Recamán Visualizer
+//
+//  Created by Richard Homan on 9/14/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // Setup code that might create autoreleased objects goes here.
+	}
+	return NSApplicationMain(argc, argv);
+}
